735cbb81e0b48257cb9d186a791f2bd3fc4194bf
JuanSuarez <jp.juansuarez@gmail.com>
2024-03-13 18:43:52 -0300

    Changed NavBar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ export default function Navbar() {
     
     return (
       <div style={styles.navbar} className="navbar">
-        <button style={{backgroundColor: 'blue', color: 'white'}}>Home</button>
-        <button style={{backgroundColor: 'green', color: 'white'}}>About</button>
-        <button style={{backgroundColor: 'red', color: 'white'}}>Contact</button> 
+        <button>Home</button>
+        <button>Projects</button>
+        <button>Contact</button> 
       </div>
     );
   }
@@ -13,22 +13,9 @@ export default function Navbar() {
       display: 'flex',
       width: '100%',
       height: '50px',
-      alignItems: 'center',
-      color: 'white',
-      fontSize: '20px',
-      fontWeight: 'bold',
-      padding: '10px',
       position: 'fixed',
       top: '0',
-      left: '0',
       right: '0',
-      zIndex: '1',
-      overflow: 'hidden',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-      alignContent: 'center',
-      alignSelf: 'center',
-      flex: '1',
       justifyContent: 'space-evenly' 
     }
   }
